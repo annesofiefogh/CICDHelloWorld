027bfd7760946c839f4e3143bbb50c77a909e734
annesofiefogh <anne2z08@stud.kea.dk>
2022-09-16 10:16:23 +0200

    Properties change again

--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -3,6 +3,10 @@ package com.example.cicd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+//Test skal udkommenteres ellers brokker Maven sig - test applikationen prøver at køre i Maven inden projektet
+//er deployet til Azure. Det skaber konflikt.
+
 //@SpringBootTest
 class CicdApplicationTests {
 
